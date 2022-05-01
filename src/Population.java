5142183dd823d28737220240284c9be15767f8f4
luigi <luigi.pizza2002@gmail.com>
2022-05-01 13:09:05 +0200

    modified Population

--- src/Population.java
+++ src/Population.java
@@ -1,15 +1,37 @@
+import java.util.Random;
+
 public class Population {
     public int people;
     public boolean sterility = false;
     public boolean noise = true;
 
-    public static void main(String[] args) {
 
-    }
-
-    public void newborn(){
+    public void newborn(Man man, Woman woman){
         if (!sterility){
             people++;
+            if (man.getClass().getName() == "Faithful")  {
+                int m = 0;
+            }
+            else {
+                int m = 1;
+            }
+
+            if (woman.getClass().getName() == "Coy")  {
+                int w = 0;
+            }
+            else {
+                int w = 1;
+            }
+
+            Random rand = new Random();
+            boolean sex = rand.nextBoolean();
+            int type = Math.round();
+            if (sex) {
+
+            }
+            else {
+
+            }
         }
 
     }
