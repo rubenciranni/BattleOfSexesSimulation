3afdaba87eeb3f3aee444000d35b2343623bfc30
gabrimat <gabrimat1@protonmail.ch>
2022-05-01 12:37:19 +0200

    Added methods to man and woman interfaces

--- src/Man.java
+++ src/Man.java
@@ -1,2 +1,3 @@
 public interface Man extends Person{
+    void propose(Woman w);
 }
--- src/Woman.java
+++ src/Woman.java
@@ -1,2 +1,5 @@
-public interface Woman {
+public interface Woman extends Person {
+    boolean acceptPropose(Man m);
+    void generateChildWith(Man m);
+
 }
