fd192d450105a1d0aa499160e82bffa0d3fbbb7e
rubenciranni <rubenciranni6@gmail.com>
2022-05-06 20:02:14 +0200

    Updated Constructors of Coy, Faithful, Fast, Philanderer

--- src/FastPopulation.java
+++ src/FastPopulation.java
@@ -6,13 +6,14 @@ public class FastPopulation extends ThreadGroup{
         this.size = size;
 
         for(int i = 0; i < size; i++) {
-            new FastPopulation.Fast(this, NameGenerator.generateFastName(i));
+            Population population = (Population) this.getParent().getParent();
+            population.initialPopulationList.add(new FastPopulation.Fast(this, RandomNameGenerator.randomNameOfGirl()));
         }
     }
 
     public class Fast extends Thread{
         public Fast(ThreadGroup group, String name) {
-            super(group, name);
+            super(group, name + " (Fast)");
         }
 
         @Override
