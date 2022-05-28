78ea581f806d19a7e918765828e27aad0e2a5877
luigi <luigi.pizza2002@gmail.com>
2022-05-28 10:14:07 +0200

    Grim Reaper ends the execution if a certain efficience is achieved

--- src/GrimReaper.java
+++ src/GrimReaper.java
@@ -1,14 +1,24 @@
+import java.util.HashMap;
+
 public class GrimReaper extends Thread {
     Population population;
     int maxValue;
+    static int callsToGrim;
+    static HashMap PreviousState;
+    static float wait;
 
     public GrimReaper(Population population, int maxValue) {
         this.population = population;
         this.maxValue = maxValue;
+        if (callsToGrim == 0) {
+            wait = maxValue/10;
+        }
+        callsToGrim+=1;
     }
 
     @Override
     public void run() {
+        PreviousState = population.getGlobalState();
         try {
             int wait = maxValue / 10;
             while (population.size > maxValue) {
@@ -16,11 +26,23 @@ public class GrimReaper extends Thread {
                 population.sterility = true;
                 sleep(wait);
                 population.sterility= false;
-                wait+= (maxValue- population.size)/10;
+                wait += (maxValue - population.size)/10;
             }
         }
-        catch (InterruptedException e) {
-
+        catch (InterruptedException e) {}
+        if (callsToGrim > maxValue/50) {
+            float Difference[] = new float[4];
+            int n=0;
+            HashMap ActiveState = population.getGlobalState();
+            float sum=0;
+            for (Object i : ActiveState.keySet()) {
+                Difference[n] = (float) ActiveState.get((String) i)-(float) PreviousState.get((String) i);
+                sum += Difference[n++];
+            }
+            if (sum < 1/maxValue) {
+                System.out.println("the sum of the differences is" +sum+ "; while each different difference is" +Difference);
+                population.sterility=true;
+            }
         }
     }
 }
