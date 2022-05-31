ce38068efa9f1f8f3a8b7e938fbec9a8aaa78608
luigi <luigi.pizza2002@gmail.com>
2022-05-31 16:09:12 +0200

    JK i found it

--- src/GrimReaper.java
+++ src/GrimReaper.java
@@ -1,58 +1,72 @@
 import java.util.HashMap;
-
 import static java.lang.Math.abs;
+
 
 public class GrimReaper extends Thread {
     Population population;
     int maxValue;
     static int callsToGrim;
+    static HashMap PreviousState;
     static int wait;
     static boolean finished;
-    int sleepingTime = 100;
+
+
 
     public GrimReaper(Population population, int maxValue) {
         this.population = population;
         this.maxValue = maxValue;
         if (callsToGrim == 0) {
-            wait = maxValue/30;
+            wait = maxValue/10;
         }
+        System.out.println("\n\n"+callsToGrim);
         callsToGrim+=1;
     }
 
     @Override
     public void run() {
-        HashMap PreviousState = population.getPopulationsToPrint();
         try {
-            sleep(sleepingTime);
-            HashMap ActiveState = population.getPopulationsToPrint();
-            HashMap Ratios = population.getGlobalStateToPrint();
-            if (callsToGrim > 10+maxValue/50) {
-                float Differences[] = new float[4];
+            PreviousState = population.getGlobalState();
+            sleep(50);
+            if (callsToGrim > 10 + maxValue / 100) {
+                float Difference[] = new float[4];
                 int n = 0;
-                int sum = 0;
+                HashMap ActiveState = population.getGlobalState();
+                float sum = 0;
                 for (Object i : ActiveState.keySet()) {
-                    Differences[n] = (int) ActiveState.get((String) i) - (int) PreviousState.get((String) i);
-                    sum += abs(Differences[n++]);
+                    Difference[n] = (float) ActiveState.get((String) i) - (float) PreviousState.get((String) i);
+                    sum += abs(Difference[n++]);
                 }
-                n=0;
-                if (sum < maxValue/15) {
-                    System.out.println("the sum of the differences is " + sum + ";\nwhile each difference is:\n");
-                    for (Object i : ActiveState.keySet()) {
-                        System.out.println(i +""+ ActiveState.get(i) + "\t\t" + PreviousState.get(i)+"\t\t"+Differences[n++]+"\t\t\t"+Ratios.get(i));
-                    }
+                if (sum < 5 / (100+(float) maxValue)) {
                     population.modifySterility(true, true);
+                    System.out.println("the sum of the differences is " + sum + ";\nEach difference is:");
+                    n = 0;
+                    System.out.println("Faithful:\t\t" +Difference[0]+ "\nCoy:\t\t\t"+Difference[1]+"\nFast:\t\t\t"+Difference[2]+"\nPhilanderers:\t"+Difference[3]);
+                    System.out.println("\n\nFinal Values:");
+                    for (Object i : ActiveState.keySet()) {
+                        System.out.println(i +": "+ ActiveState.get((String)i));
+                    }
+                    System.out.println("\n");
+                    Simulator.Time = 1000;
                     Simulator.print = true;
                 }
             }
-            while (population.size > maxValue) {
-                population.modifySterility(true, false);
-                sleep(wait);
-                population.modifySterility(false, false);
+            if (!finished) {
+                while (population.size > maxValue) {
+                    population.modifySterility(true, false);
+                    sleep(wait);
+                    population.modifySterility(false, false);
+                }
+                int size2 = population.size;
+                if (size2>1+4*maxValue/5) {
+                    wait += (maxValue - size2) / 10;
+                }
+                else {
+                    wait += (size2 - maxValue) / 12;
+                }
+                System.out.println(wait);
+                SubWomanPopulation.WomanSubType.grimmyIsOut = false;
             }
-            System.out.println(callsToGrim+"\n"+wait+"\n\n");
-            SubWomanPopulation.WomanSubType.grimmyIsOut = false;
         }
-        catch (InterruptedException e) {}
+        catch  (InterruptedException e) {}
     }
 }
-
